05392f01d4be00c9f8c5e59ef6d260d438e3b23d
Dickson Lee <dicksonlee009@gmail.com>
2019-06-28 12:43:30 +0800

    update until token authorization

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { createMessage } from "./messages";
+import { createMessage, returnErrors } from "./messages";
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 export const getLeads = () => dispatch => {
   axios
@@ -12,9 +12,12 @@ export const getLeads = () => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
+//DELETE LEAD
 export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}`)
@@ -28,6 +31,7 @@ export const deleteLead = id => dispatch => {
     .catch(err => console.log(err));
 };
 
+//ADD LEAD
 export const addLead = lead => dispatch => {
   axios
     .post("/api/leads/", lead)
@@ -38,14 +42,7 @@ export const addLead = lead => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };

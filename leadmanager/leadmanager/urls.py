f7dc8f78ca6cd8236a5dbecb5187a946c0a829e2
Dickson Lee <dicksonlee_8016@hotmail.com>
2019-07-18 09:11:11 +0800

    new repo name for remote repo

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("", include("frontend.urls")),
     path("", include("leads.urls")),
     path("", include("accounts.urls")),
+    path("admin/",admin.site.urls),
 ]
--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -2,15 +2,14 @@ from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
+
 class LeadViewSet(viewsets.ModelViewSet):
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
-    
+    permission_classes = [permissions.DjangoModelPermissions]
+
     serializer_class = LeadSerializer
 
     def get_queryset(self):
         return self.request.user.leads.all()
 
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
